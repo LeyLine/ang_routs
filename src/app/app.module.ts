21dc5e998de5ed62b919b063f0cf7c8c1ca39b04
Adrian Llerena <a.llerena001@gmail.com>
2018-08-08 20:15:32 -0400

    seond it done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from "@angular/router";
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyAboutComponent } from './my-about/my-about.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+
+const routes: Routes = [
+  { path: '', component: ContactListComponent  },
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'about', component: MyAboutComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponent,
+    MyAboutComponent,
+    ContactListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
